681f67750199f8c12cb58aedb6f24991d8233e3c
Mateusz <mateusz.oracz2001@gmail.com>
2025-10-04 11:38:28 +0200

    render test 4

--- app/api/routes/root.py
+++ app/api/routes/root.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 @router.get("/", response_model=Message, tags=["root"])
 async def root():
-    return Message(message="Hello World version 1.0.3")
+    return Message(message="Hello World version 1.0.4")
 
 
 @router.get("/hello/{name}", response_model=Message, tags=["root"])
